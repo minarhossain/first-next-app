f8c8150ed26d0a59904416869eb4cc5d963618e6
minarhossain <codewithmnr@gmail.com>
2024-03-24 18:17:59 +0600

    [processing] next app for me

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl">I love bangladesh</h1>
+      <h1>hello man</h1>
+      <h3>I am here no fear raj is here</h3>
+
+      <img src="" alt="" />
+    </main>
+  );
+}
